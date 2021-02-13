fa55d90567c89265c970c0c4554122e756cc1e4d
LAPTOP-2MP57682\Pa-De <pa-deasy@hotmail.com>
2021-02-13 12:19:39 -0800

    Palindrome checker

---
+++ PalindromeChecker.Tests.Unit/PalindromeExtensionsTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace PalindromeChecker.Tests.Unit
+{
+    [TestFixture]
+    public class PalindromeExtensionsTests
+    {
+        [TestCase("aba")]
+        [TestCase("aa")]
+        [TestCase("baab")]
+        public void Given_String_When_IsAPalindrome_Then_ReturnsTrue(string input)
+        {
+            input.IsAPalindrome().Should().BeTrue();
+        }
+
+        [TestCase("iop")]
+        [TestCase("na")]
+        public void Given_String_When_IsNotAPalindrome_Then_ReturnsFalse(string input)
+        {
+            input.IsAPalindrome().Should().BeFalse();
+        }
+
+        [TestCase("aa", 1)]
+        [TestCase("aaa", 3)]
+        [TestCase("abaab", 3)]
+        [TestCase("abbaeae", 4)]
+        public void Given_String_When_PalindromesExist_Then_ReturnsCorrectCount(string input, int expectedCount)
+        {
+            input.CountOfPalindromes().Should().Be(expectedCount);
+        }
+    }
+}
