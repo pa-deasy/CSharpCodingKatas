7fe673951ded1bdbfd157c941e465f971e9e60ef
LAPTOP-2MP57682\Pa-De <pa-deasy@hotmail.com>
2021-02-13 21:54:22 -0800

    Add circular linked lists

---
+++ CircularLinkedListExamples.Tests.Unit/CircularLinkedListTests.cs
@@ -0,0 +1,175 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CircularLinkedListExamples.Tests.Unit
+{
+    [TestFixture]
+    public class CircularLinkedListTests
+    {
+        [Test]
+        public void Given_CircularLinkedList_When_PositionSearched_Then_ExpectedNodeReturned()
+        {
+            var circularLinkedList = new CircularLinkedList();
+
+            var first = new Node(1);
+            var second = new Node(2);
+            var third = new Node(3);
+            var fourth = new Node(4);
+
+            circularLinkedList.Head = first;
+            circularLinkedList.Head.Next = second;
+            second.Next = third;
+            third.Next = fourth;
+            fourth.Next = circularLinkedList.Head;
+
+            circularLinkedList.PositionAt(0).Value.Should().Be(1);
+            circularLinkedList.PositionAt(3).Value.Should().Be(4);
+            circularLinkedList.PositionAt(5).Should().BeNull();
+        }
+
+        [Test]
+        public void Given_CircularLinkedList_When_LastNodeRetrieved_Then_ExpectedNode()
+        {
+            var circularLinkedList = new CircularLinkedList();
+
+            var first = new Node(1);
+            var second = new Node(2);
+            var third = new Node(3);
+            var fourth = new Node(4);
+
+            circularLinkedList.Head = first;
+            circularLinkedList.Head.Next = second;
+            second.Next = third;
+            third.Next = fourth;
+            fourth.Next = circularLinkedList.Head;
+
+            circularLinkedList.LastNode().Value.Should().Be(4);
+        }
+
+        [Test]
+        public void Given_CircularLinkedListWithOneNode_When_LastNodeRetrieved_Then_ExpectedNode()
+        {
+            var circularLinkedList = new CircularLinkedList();
+
+            var first = new Node(1);
+
+            circularLinkedList.Head = first;
+            circularLinkedList.Head.Next = circularLinkedList.Head;
+
+            circularLinkedList.LastNode().Value.Should().Be(1);
+        }
+
+        [Test]
+        public void Given_CircularList_When_NodesPushed_Then_PresentInListAsExpected()
+        {
+            var circularLinkedList = new CircularLinkedList();
+
+            circularLinkedList.Push(new Node(3));
+            circularLinkedList.Push(new Node(2));
+            circularLinkedList.Push(new Node(1));
+
+            circularLinkedList.PositionAt(0).Value.Should().Be(1);
+            circularLinkedList.PositionAt(1).Value.Should().Be(2);
+            circularLinkedList.PositionAt(2).Value.Should().Be(3);
+        }
+
+        [Test]
+        public void Given_EmptyCircularList_When_SingleNodePushed_Then_ListIsAsExpected()
+        {
+            var circularLinkedList = new CircularLinkedList();
+
+            circularLinkedList.Push(new Node(1));
+
+            circularLinkedList.PositionAt(0).Value.Should().Be(1);
+            circularLinkedList.PositionAt(1).Should().BeNull();
+            circularLinkedList.LastNode().Value.Should().Be(1);
+        }
+
+        [Test]
+        public void Given_CicularLinkedList_When_SecondLastNodeRetrieved_Then_AsExpected()
+        {
+            var circularLinkedList = new CircularLinkedList();
+
+            circularLinkedList.Push(new Node(3));
+            circularLinkedList.Push(new Node(2));
+            circularLinkedList.Push(new Node(1));
+
+            circularLinkedList.SecondLastNode().Value.Should().Be(2);
+        }
+
+        [Test]
+        public void Given_CicularLinkedListWith2Node_When_SecondLastNodeRetrieved_Then_AsExpected()
+        {
+            var circularLinkedList = new CircularLinkedList();
+
+            circularLinkedList.Push(new Node(2));
+            circularLinkedList.Push(new Node(1));
+
+            circularLinkedList.SecondLastNode().Value.Should().Be(1);
+        }
+
+        [Test]
+        public void Given_CicularLinkedListWith1Node_When_SecondLastNodeRetrieved_Then_AsExpected()
+        {
+            var circularLinkedList = new CircularLinkedList();
+
+            circularLinkedList.Push(new Node(1));
+
+            circularLinkedList.SecondLastNode().Value.Should().Be(1);
+        }
+
+        [Test]
+        public void Given_CircularLinkedList_When_FirstSwappedWithLast_Then_NodesAsExpected()
+        {
+            var circularLinkedList = new CircularLinkedList();
+
+            circularLinkedList.Push(new Node(5));
+            circularLinkedList.Push(new Node(4));
+            circularLinkedList.Push(new Node(3));
+            circularLinkedList.Push(new Node(2));
+            circularLinkedList.Push(new Node(1));
+
+            circularLinkedList.PositionAt(0).Value.Should().Be(1);
+            circularLinkedList.PositionAt(4).Value.Should().Be(5);
+
+            circularLinkedList.SwapFirstAndLast();
+
+            circularLinkedList.PositionAt(0).Value.Should().Be(5);
+            circularLinkedList.PositionAt(4).Value.Should().Be(1);
+        }
+
+        [Test]
+        public void Given_CircularLinkedListWith2Node_When_FirstSwappedWithLast_Then_NodesAsExpected()
+        {
+            var circularLinkedList = new CircularLinkedList();
+
+            circularLinkedList.Push(new Node(2));
+            circularLinkedList.Push(new Node(1));
+
+            circularLinkedList.PositionAt(0).Value.Should().Be(1);
+            circularLinkedList.PositionAt(1).Value.Should().Be(2);
+
+            circularLinkedList.SwapFirstAndLast();
+
+            circularLinkedList.PositionAt(0).Value.Should().Be(2);
+            circularLinkedList.PositionAt(1).Value.Should().Be(1);
+        }
+
+        [Test]
+        public void Given_CircularLinkedListWith1Node_When_FirstSwappedWithLast_Then_NodesAsExpected()
+        {
+            var circularLinkedList = new CircularLinkedList();
+
+            circularLinkedList.Push(new Node(1));
+
+            circularLinkedList.PositionAt(0).Value.Should().Be(1);
+
+            circularLinkedList.SwapFirstAndLast();
+
+            circularLinkedList.PositionAt(0).Value.Should().Be(1);
+        }
+    }
+}
---
+++ CircularLinkedListExamples/CircularLinkedList.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CircularLinkedListExamples
+{
+    public class CircularLinkedList
+    {
+        public Node Head { get; set; }
+
+        public Node PositionAt(int position)
+        {
+            var currentPosition = 0;
+            var currentNode = Head;
+
+            while(currentPosition < position)
+            {
+                currentNode = currentNode.Next;
+                currentPosition++;
+
+                if (position > 0 && currentNode == Head)
+                    return null;
+            }
+
+            return currentNode;
+        }
+
+        public Node LastNode()
+            => LastNode(Head);
+
+        private Node LastNode(Node node)
+        {
+            if (node.Next == Head)
+                return node;
+
+            return LastNode(node.Next);
+        }
+
+        public Node SecondLastNode()
+            => SecondLastNode(Head);
+
+        private Node SecondLastNode(Node node)
+        {
+            if (node.Next.Next == Head)
+                return node;
+
+            return SecondLastNode(node.Next);
+        }
+
+        public void Push(Node node)
+        {
+            if (Head == null)
+            {
+                Head = node;
+                Head.Next = Head;
+                return;
+            }
+
+            var lastNode = LastNode();
+            node.Next = Head;
+            Head = node;
+            lastNode.Next = Head;
+        }
+
+        public void SwapFirstAndLast()
+        {
+            var tmpNode = Head;
+            var lastNode = LastNode();
+            var secondLastNode = SecondLastNode();
+
+            if(secondLastNode == Head)
+            {
+                Head = lastNode;
+                Head.Next = tmpNode;
+            }
+            else
+            {
+                Head = lastNode;
+                Head.Next = tmpNode.Next;
+                secondLastNode.Next = tmpNode;
+            }
+            
+            tmpNode.Next = Head;
+        }
+    }
+}
