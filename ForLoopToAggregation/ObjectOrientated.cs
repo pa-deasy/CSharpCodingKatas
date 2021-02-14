066fe53e4e0cf83b6f1535b752ef059634f19d50
LAPTOP-2MP57682\Pa-De <pa-deasy@hotmail.com>
2021-02-13 22:10:29 -0800

    Convert a for loop to use aggregation

---
+++ ForLoopToAggregation/ObjectOrientated.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForLoopToAggregation
+{
+    public class ObjectOrientated
+    {
+        public static int countingValleys(int steps, string path)
+        {
+            var pathSteps = path.ToCharArray();
+            var seaLevel = 0;
+            var numberOfvalleys = 0;
+
+            foreach (char pathStep in pathSteps)
+            {
+                seaLevel = SeaLevelFrom(pathStep, seaLevel);
+
+                if (EnteredValley(pathStep, seaLevel))
+                    numberOfvalleys++;
+            }
+
+            return numberOfvalleys;
+        }
+
+        private static int SeaLevelFrom(char pathStep, int seaLevel)
+        {
+            if (pathStep == 'D')
+                seaLevel -= 1;
+
+            if (pathStep == 'U')
+                seaLevel += 1;
+
+            return seaLevel;
+        }
+
+        private static bool EnteredValley(char pathStep, int seaLevel)
+            => pathStep == 'D' && seaLevel == -1;
+    }
+}
