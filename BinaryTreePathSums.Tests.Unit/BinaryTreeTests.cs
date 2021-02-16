1210f23c97e1d9adcadcf89908062e2bce018fec
LAPTOP-2MP57682\Pa-De <pa-deasy@hotmail.com>
2021-02-15 20:35:56 -0800

    Add example to sum from root

---
+++ BinaryTreePathSums.Tests.Unit/BinaryTreeTests.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace BinaryTreePathSums.Tests.Unit
+{
+    [TestFixture]
+    public class BinaryTreeTests
+    {
+        [Test]
+        public void Given_BinaryTree_When_OnePathSumsToNumber_Then_PathIsFound()
+        {
+            var paths = TestBinaryTreeA.PathsFromRootSummingTo(8);
+
+            paths.Single()[0].Should().Be(1);
+            paths.Single()[1].Should().Be(3);
+            paths.Single()[2].Should().Be(4);
+        }
+
+        [Test]
+        public void Given_BinaryTree_WhenTwoPathsSumsToNumber_Then_PathsAreFound()
+        {
+            var paths = TestBinaryTreeB.PathsFromRootSummingTo(38);
+
+            paths.Count.Should().Be(2);
+
+            paths.First()[0].Should().Be(10);
+            paths.First()[1].Should().Be(28);
+
+            paths.Last()[0].Should().Be(10);
+            paths.Last()[1].Should().Be(13);
+            paths.Last()[2].Should().Be(15);
+        }
+
+        private BinaryTree TestBinaryTreeA =>
+            new BinaryTree(
+                           new Node(1,
+            new Node(20),             new Node(3,
+                                  new Node(4, 
+                          new Node(6), new Node(7)),        new Node(15, 
+                                                      new Node(8), new Node(9)))));
+
+        private BinaryTree TestBinaryTreeB =>
+            new BinaryTree(
+                           new Node(10,
+            new Node(28),            new Node(13,
+                               new Node(14,
+                       new Node(21), new Node(22)),         new Node(15,
+                                                      new Node(23), new Node(24)))));
+    }
+}
---
+++ BinaryTreePathSums/BinaryTree.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTreePathSums
+{
+    public class BinaryTree
+    {
+        public Node Root { get; private set; }
+
+        public BinaryTree(Node root)
+        {
+            Root = root;
+        }
+
+        public List<List<int>> PathsFromRootSummingTo(int sum)
+            => SumOfPathsFrom(Root, sum, SumState.New).SuccessfulPaths;
+
+        public SumState SumOfPathsFrom(Node node, int targetSum, SumState sumState)
+        {
+            if (node == null || sumState.CurrentSum + node.Value > targetSum)
+                return sumState;
+
+            var currentPath = new List<int> (sumState.CurrentPath);
+            var currentSum = sumState.CurrentSum;
+            var successfulPaths = sumState.SuccessfulPaths;
+
+            currentSum += node.Value;
+            currentPath.Add(node.Value);
+
+            if (currentSum == targetSum)
+            {
+                successfulPaths.Add(currentPath);
+                return new SumState(sumState.CurrentSum, sumState.CurrentPath, successfulPaths);
+            }
+
+            if (node.Left != null)
+                successfulPaths = SumOfPathsFrom(node.Left, targetSum, new SumState(currentSum, currentPath, successfulPaths)).SuccessfulPaths;
+
+            if (node.Right != null)
+                successfulPaths = SumOfPathsFrom(node.Right, targetSum, new SumState(currentSum, currentPath, successfulPaths)).SuccessfulPaths;
+
+            return new SumState(sumState.CurrentSum, sumState.CurrentPath, successfulPaths);
+        }
+    }
+
+    public class SumState
+    {
+        public int CurrentSum { get; private set; }
+        public List<int> CurrentPath { get; private set; }
+        public List<List<int>> SuccessfulPaths { get; private set; }
+
+        public SumState(int currentSum, List<int> currentPath, List<List<int>> successfulPaths)
+        {
+            CurrentSum = currentSum;
+            CurrentPath = currentPath;
+            SuccessfulPaths = successfulPaths;
+        }
+
+        public static SumState New => new SumState(0, new List<int>(), new List<List<int>>());
+    }
+}
