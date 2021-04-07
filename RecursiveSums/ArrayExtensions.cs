3924a24b52d61f7620ac0e075ae66bc22033c14e
LAPTOP-2MP57682\Pa-De <pa-deasy@hotmail.com>
2021-04-06 20:04:05 -0700

    Add recursive array examples

---
+++ RecursiveSums/ArrayExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace RecursiveSums
+{
+    public static class ArrayExtensions
+    {
+        public static int Sum(this int[] numbers)
+        {
+            if (!numbers.Any())
+                return 0;
+
+            var numbersStack = new Stack<int>(numbers);
+            var popped = numbersStack.Pop();
+
+            return popped + numbersStack.ToArray().Sum();
+        }
+
+        public static int Count<T>(this T[] array)
+        {
+            if (!array.Any())
+                return 0;
+
+            var stack = new Stack<T>(array);
+            stack.Pop();
+
+            return 1 + stack.ToArray().Count();
+        }
+
+        public static int Max(this int[] numbers)
+        {
+            if (!numbers.Any())
+                return 0;
+
+            var numbersStack = new Stack<int>(numbers);
+            var popped = numbersStack.Pop();
+
+            var subsequentMax = numbersStack.ToArray().Max();
+            return popped > subsequentMax ? popped : subsequentMax;
+        }
+    }
+}
