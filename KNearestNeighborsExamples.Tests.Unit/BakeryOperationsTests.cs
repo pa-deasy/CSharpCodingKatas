0688a62ae100a05cb0f0aeb3e962f3665121614e
LAPTOP-2MP57682\Pa-De <pa-deasy@hotmail.com>
2021-04-14 22:47:42 -0700

    Examples of classification and regression using K nearest neighbor

---
+++ KNearestNeighborsExamples.Tests.Unit/BakeryOperationsTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace KNearestNeighborsExamples.Tests.Unit
+{
+    [TestFixture]
+    public class BakeryOperationsTests
+    {
+        [Test]
+        public void Given_SalesConditions_When_ClosestNeighborsObtained_Then_PredictsSales()
+        {
+            var salesRecords = new List<SalesRecord> 
+            {
+                new SalesRecord{ WeatherScore = 5, WasWeekendOrHoliday = 1, WasGameDay = 0, BreadSold = 300 },
+                new SalesRecord{ WeatherScore = 3, WasWeekendOrHoliday = 1, WasGameDay = 1, BreadSold = 225 },
+                new SalesRecord{ WeatherScore = 1, WasWeekendOrHoliday = 1, WasGameDay = 0, BreadSold = 75 },
+                new SalesRecord{ WeatherScore = 4, WasWeekendOrHoliday = 0, WasGameDay = 1, BreadSold = 200 },
+                new SalesRecord{ WeatherScore = 4, WasWeekendOrHoliday = 0, WasGameDay = 0, BreadSold = 150 },
+                new SalesRecord{ WeatherScore = 2, WasWeekendOrHoliday = 0, WasGameDay = 0, BreadSold = 50 }
+            };
+
+            Assert.AreEqual(218.75, salesRecords.RegressionForBreadSoldWith(4, 1, 0));
+        }
+    }
+}
---
+++ KNearestNeighborsExamples/BakeryOperations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KNearestNeighborsExamples
+{
+    public static class BakeryOperations
+    {
+        public static double RegressionForBreadSoldWith(this List<SalesRecord> salesRecords, int weatherScore, int isWeekendOrHoliday, int isGameDay)
+        {
+            var closest4Neighbors = salesRecords.Select(
+                s => new Neighbor<SalesRecord>
+                {
+                    Value = s,
+                    Distance = CalculateDistanceBetween(weatherScore, isWeekendOrHoliday, isGameDay, s.WeatherScore, s.WasWeekendOrHoliday, s.WasGameDay)
+                }).OrderBy(n => n.Distance).Take(4);
+
+            return closest4Neighbors.Select(n => n.Value.BreadSold).Average();
+        }
+
+        private static double CalculateDistanceBetween(int x1, int y1, int z1, int x2, int y2, int z2)
+            => Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2) + Math.Pow((z1 - z2), 2));
+    }
+
+    public class SalesRecord
+    {
+        public int WeatherScore { get; set; }
+        public int WasWeekendOrHoliday { get; set; } 
+        public int WasGameDay { get; set; }
+        public int BreadSold { get; set; }
+    }
+}
