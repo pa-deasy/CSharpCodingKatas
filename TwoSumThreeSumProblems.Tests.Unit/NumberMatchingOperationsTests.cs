f82bad681a14d263b32cddf5bd2025d92cc62d31
LAPTOP-2MP57682\Pa-De <pa-deasy@hotmail.com>
2021-04-16 21:11:44 -0700

    Find pairs and triplets summing to a target value

---
+++ TwoSumThreeSumProblems.Tests.Unit/NumberMatchingOperationsTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace TwoSumThreeSumProblems.Tests.Unit
+{
+    [TestFixture]
+    public class NumberMatchingOperationsTests
+    {
+        [Test]
+        public void Given_Target_When_TargetSummingPairExists_Then_RetursPair()
+        {
+            var numbers = new List<int> { 0, -1, 2, -3, 1 };
+            var expectedPair = new List<int> { 1, -3 };
+
+            var pair = numbers.FindPairSummingTo(-2);
+
+            Assert.AreEqual(expectedPair, pair);
+        }
+
+        [Test]
+        public void Given_Target_When_TargetSummingTripletExists_Then_RetursTriplet()
+        {
+            var numbers = new List<int> { 12, 3, 4, 1, 6, 9 };
+            var expectedPair = new List<int> { 9, 12, 3 };
+
+            var triplet = numbers.FindTripletSummingTo(24);
+
+            Assert.AreEqual(expectedPair, triplet);
+        }
+    }
+}
---
+++ TwoSumThreeSumProblems/NumberMatchingOperations.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace TwoSumThreeSumProblems
+{
+    public static class NumberMatchingOperations
+    {
+        public static List<int> FindPairSummingTo(this List<int> numbers, int target)
+        {
+            var numbersSet = new HashSet<int>();
+
+            foreach (int currentNumber in numbers)
+            {
+                var diff = target - currentNumber;
+
+                if (numbersSet.Contains(diff))
+                    return new List<int> { currentNumber, diff };
+
+                numbersSet.Add(currentNumber);
+            }
+
+            return new List<int>();
+        }
+
+        public static List<int> FindTripletSummingTo(this List<int> numbers, int target)
+        {
+            var numbersSet = new HashSet<int>();
+
+            foreach(int currentNumber in numbers)
+            {
+                foreach(int setNumber in numbersSet)
+                {
+                    var diff = target - currentNumber - setNumber;
+                    if (numbersSet.Contains(diff))
+                        return new List<int> { currentNumber, setNumber, diff };
+                }
+                numbersSet.Add(currentNumber);
+            }
+
+            return new List<int>();
+        }
+    }
+}
