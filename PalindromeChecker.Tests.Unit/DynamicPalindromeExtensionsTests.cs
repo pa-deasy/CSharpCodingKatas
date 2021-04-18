06623fff799c9f55946498eaf5e17ee46104c62a
LAPTOP-2MP57682\Pa-De <pa-deasy@hotmail.com>
2021-04-18 13:30:26 -0700

    Add DP function to calculate longestest substring

---
+++ PalindromeChecker.Tests.Unit/DynamicPalindromeExtensionsTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace PalindromeChecker.Tests.Unit
+{
+    [TestFixture]
+    public class DynamicPalindromeExtensionsTests
+    {
+        [TestCase("forgeeksskeegfor", "geeksskeeg")]
+        [TestCase("Geeks", "ee")]
+        public void Given_Word_When_PalindromeExists_Then_ReturnsLongestPalindrome(string word, string expectedString)
+        {
+            word.LongestPalindrome().Should().Be(expectedString);
+        }
+    }
+}
