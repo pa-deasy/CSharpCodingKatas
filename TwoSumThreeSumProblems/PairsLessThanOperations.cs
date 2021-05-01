0ef9beca104c3d690aaa4a1cb1ad831678587b04
LAPTOP-2MP57682\Pa-De <pa-deasy@hotmail.com>
2021-04-30 17:27:27 -0700

    Method to find all pairs less than x in sorted array

---
+++ TwoSumThreeSumProblems/PairsLessThanOperations.cs
@@ -0,0 +1,29 @@
+﻿namespace TwoSumThreeSumProblems
+{
+    public static class PairsLessThanOperations
+    {
+        public static int CountOfPairsLessThan(this int[] numbers, int target)
+        {
+            var totalPairs = 0;
+
+            for(int lowerIndex = 0, higerIndex = numbers.Length - 1; lowerIndex < numbers.Length;)
+            {
+                var total = numbers[higerIndex] + numbers[lowerIndex];
+
+                if (lowerIndex == higerIndex)
+                    break;
+
+                if (total >= target)
+                {
+                    higerIndex--;
+                    continue;
+                }
+
+                totalPairs += (higerIndex - lowerIndex);
+                lowerIndex++;
+            }
+
+            return totalPairs;
+        }
+    }
+}
