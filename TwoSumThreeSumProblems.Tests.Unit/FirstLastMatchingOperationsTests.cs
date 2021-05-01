68a8b50873f700555ca73589f1a9ed161cbf3041
LAPTOP-2MP57682\Pa-De <pa-deasy@hotmail.com>
2021-05-01 11:06:39 -0700

    Find count of matching first and last digits in range

---
+++ TwoSumThreeSumProblems.Tests.Unit/FirstLastMatchingOperationsTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace TwoSumThreeSumProblems.Tests.Unit
+{
+    [TestFixture]
+    public class FirstLastMatchingOperationsTests
+    {
+        [TestCase(7, 20, 4)]
+        [TestCase(7, 68, 9)]
+        [TestCase(5, 40, 8)]
+        public void Given_SimpleNumberRange_When_MatchingFirstAndLastSought_Then_ReturnsCountOfMatches(int start, int end, int expected)
+        {
+            Assert.AreEqual(expected, FirstLastMatchingOperations.FirstLastMatchingInRange(start, end));
+        }
+    }
+}
