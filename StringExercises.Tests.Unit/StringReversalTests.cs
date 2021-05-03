35b7560ea9f332856b18d387bda6c5ef25cc47a6
LAPTOP-2MP57682\Pa-De <pa-deasy@hotmail.com>
2021-05-03 11:00:20 -0700

    Add method to reverse a sentence

---
+++ StringExercises.Tests.Unit/StringReversalTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace StringExercises.Tests.Unit
+{
+    [TestFixture]
+    public class StringReversalTests
+    {
+        [Test]
+        public void Given_Sentence_When_Reversed_Then_AsExpected()
+        {
+            var sentence = "My name is Khan";
+            var expected = "Khan is name My";
+
+            var reversed = sentence.ReverseSentence();
+
+            Assert.AreEqual(expected, reversed);
+        }
+    }
+}
