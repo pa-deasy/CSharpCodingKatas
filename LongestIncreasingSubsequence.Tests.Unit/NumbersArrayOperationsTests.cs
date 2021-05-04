e2a1a472f35d7de702b2ccd4b377fe2d932c278f
LAPTOP-2MP57682\Pa-De <pa-deasy@hotmail.com>
2021-05-04 12:23:23 -0700

    Dynamic programming example for longest increasing subsequence

---
+++ LongestIncreasingSubsequence.Tests.Unit/NumbersArrayOperationsTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace LongestIncreasingSubsequence.Tests.Unit
+{
+    [TestFixture]
+    public class NumbersArrayOperationsTests
+    {
+        [Test]
+        public void Given_NumbersSetA_When_LongestSubsequenceObtained_Then_IsAsExpected()
+        {
+            var numbers = new int[] { 3, 10, 2, 1, 20 };
+
+            Assert.AreEqual(3, numbers.LongestSubsequence());
+        }
+
+        [Test]
+        public void Given_NumbersSetB_When_LongestSubsequenceObtained_Then_IsAsExpected()
+        {
+            var numbers = new int[] { 3, 2 };
+
+            Assert.AreEqual(1, numbers.LongestSubsequence());
+        }
+
+        [Test]
+        public void Given_NumbersSetC_When_LongestSubsequenceObtained_Then_IsAsExpected()
+        {
+            var numbers = new int[] { 50, 3, 10, 7, 40, 80 };
+
+            Assert.AreEqual(4, numbers.LongestSubsequence());
+        }
+    }
+}
---
+++ LongestIncreasingSubsequence/NumbersArrayOperations.cs
@@ -0,0 +1,27 @@
+﻿namespace LongestIncreasingSubsequence
+{
+    public static class NumbersArrayOperations
+    {
+        public static int LongestSubsequence(this int[] numbers)
+        {
+            var calculations = new int[numbers.Length];
+
+            for (int index = 0; index < calculations.Length; index++)
+                calculations[index] = 1;
+
+            for (int fixedIndex = 1; fixedIndex < calculations.Length; fixedIndex++)
+                for(int compareIndex = 0; compareIndex < fixedIndex; compareIndex++)
+                {
+                    if (numbers[fixedIndex] > numbers[compareIndex] && calculations[compareIndex] + 1 > calculations[fixedIndex])
+                        calculations[fixedIndex] = calculations[compareIndex] + 1;
+                }
+
+            var longest = 1;
+            foreach (int sequenceCount in calculations)
+                if (sequenceCount > longest)
+                    longest = sequenceCount;
+
+            return longest;
+        }
+    }
+}
