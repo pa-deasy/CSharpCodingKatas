a09166cb4fe21a8219c599a43d9f8e6756570bed
LAPTOP-2MP57682\Pa-De <pa-deasy@hotmail.com>
2021-05-06 16:16:03 -0700

    Add method to identify missing number in sequence and a method to identify a majority element

---
+++ MajorityElement/NumbersArrayOperations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MajorityElement
+{
+    public static class NumbersArrayOperations
+    {
+        public static int MajorityElement(this int[] numbers)
+        {
+            var elements = new Dictionary<int, int>();
+            int? maxElement = null;
+
+            foreach (int number in numbers)
+            {
+                if (elements.ContainsKey(number))
+                    elements[number] += 1;
+
+                else
+                    elements.Add(number, 1);
+
+                if (maxElement == null || elements[maxElement.Value] < elements[number])
+                    maxElement = number;
+            }
+
+            if (elements[maxElement.Value] > numbers.Length / 2)
+            {
+                Console.WriteLine($"Majority element is {maxElement}");
+                return maxElement.Value;
+            }
+
+            Console.WriteLine($"No majority element");
+            return 0;
+        }
+    }
+}
---
+++ MissingNumber.Tests.Unit/NumbersArrayOperationsTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace MissingNumber.Tests.Unit
+{
+    [TestFixture]
+    public class NumbersArrayOperationsTests
+    {
+        [Test]
+        public void Given_ArrayOfNumbers_When_ContainsMissingNumber_Then_ReturnsExpected()
+        {
+            var numbers = new int[] { 1, 2, 4, 6, 3, 7, 8 };
+
+            Assert.AreEqual(5, numbers.MissingNumber());
+        }
+    }
+}
